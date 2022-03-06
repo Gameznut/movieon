b811f9f6568571775c513f87f4b5816cc74efa9b
emmanuel apabiekun <31988283+Gameznut@users.noreply.github.com>
2022-03-06 19:00:01 +0100

    nuxt deploy

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex relative">
+  <div class="flex relative bg-black">
     <Nav class="fixed" />
     <Display class="w-9/12 ml-56 px-12 border-l border-r border-gray-400" />
     <Nav class="w-96 fixed navright" />
