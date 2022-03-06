d898705b6b555f8fa856c4d8939b2ed53b2cc8aa
emmanuel apabiekun <31988283+Gameznut@users.noreply.github.com>
2022-03-06 19:47:42 +0100

    nuxt deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,14 +34,10 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     '@/assets/css/main.css',
-    '@splidejs/splide/dist/css/splide.min.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [{
-    src: '~plugins/slider.js',
-    mode: 'client'
-  }],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -67,6 +63,4 @@ export default {
       },
     },
   },
-  target: 'static',
-  ssr: "false",
 }
